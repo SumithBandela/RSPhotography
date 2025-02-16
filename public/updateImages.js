c40f536244f5e04aaf5cf0b1a9cd6e2357e06227
Narain K <knarain42@yahoo.com>
2025-02-16 19:58:58 +0530

    modified add images

--- public/updateImages.js
+++ public/updateImages.js
@@ -1,3 +1,4 @@
+// filepath: /c:/codeBaseDevelopment/RSPhotography/updateAlbums.js
 const fs = require('fs');
 const path = require('path');
 const chokidar = require('chokidar');
@@ -6,7 +7,7 @@ const imagesFolder = path.join(__dirname, 'public', 'images');
 const jsonFilePath = path.join(__dirname, 'public', 'images.json');
 
 // Function to update images.json
-function updateImagesJson() {
+function updateAlbumsJson(newImage) {
   fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading images.json:', err);
@@ -14,24 +15,17 @@ function updateImagesJson() {
     }
 
     const jsonData = JSON.parse(data);
-    const existingImages = jsonData.images.map(image => image.img_src);
+    const existingAlbums = jsonData.albums.map(album => album.img_src);
 
-    fs.readdir(imagesFolder, (err, files) => {
-      if (err) {
-        console.error('Error reading images folder:', err);
-        return;
-      }
-
-      files.forEach((file, index) => {
-        const imgSrc = `images/${file}`;
-        if (!existingImages.includes(imgSrc)) {
-          const newId = jsonData.images.length + 1;
-          jsonData.images.push({
-            id: newId,
-            img_src: imgSrc,
-            photos: [{ img_src: imgSrc }]
-          });
-        }
+    const imgSrc = `images/${newImage}`;
+    if (!existingAlbums.includes(imgSrc)) {
+      const newId = jsonData.albums.length + 1;
+      jsonData.albums.push({
+        id: newId,
+        title: `New Album ${newId}`,
+        description: `Description for new album ${newId}`,
+        img_src: imgSrc,
+        photos: [{ img_src: imgSrc }]
       });
 
       fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', err => {
@@ -41,12 +35,13 @@ function updateImagesJson() {
           console.log('images.json updated successfully.');
         }
       });
-    });
+    }
   });
 }
 
 // Watch for changes in the images folder
 chokidar.watch(imagesFolder).on('add', (filePath) => {
-  console.log(`File added: ${filePath}`);
-  updateImagesJson();
+  const fileName = path.basename(filePath);
+  console.log(`File added: ${fileName}`);
+  updateAlbumsJson(fileName);
 });
