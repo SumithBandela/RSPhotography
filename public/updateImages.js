f7657714849f1b8421211aff1511656745cda91c
Narain K <knarain42@yahoo.com>
2025-02-16 21:44:01 +0530

    commit

--- public/updateImages.js
+++ public/updateImages.js
@@ -1,4 +1,3 @@
-// filepath: /c:/codeBaseDevelopment/RSPhotography/updateAlbums.js
 const fs = require('fs');
 const path = require('path');
 const chokidar = require('chokidar');
@@ -7,7 +6,7 @@ const imagesFolder = path.join(__dirname, 'images');
 const jsonFilePath = path.join(__dirname, 'images.json');
 
 // Function to update images.json
-function updateAlbumsJson(newImage) {
+function updateAlbumsJson(newImagePath) {
   fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading images.json:', err);
@@ -15,18 +14,33 @@ function updateAlbumsJson(newImage) {
     }
 
     const jsonData = JSON.parse(data);
-    const existingAlbums = jsonData.albums.map(album => album.img_src);
+    const imgSrc = path.relative(path.join(__dirname, 'public'), newImagePath).replace(/\\/g, '/');
+    const subfolderPath = path.dirname(newImagePath);
+    const metadataFilePath = path.join(subfolderPath, 'metadata.json');
 
-    const imgSrc = `images/${newImage}`;
-    if (!existingAlbums.includes(imgSrc)) {
-      const newId = jsonData.albums.length + 1;
-      jsonData.albums.push({
-        id: newId,
-        title: `New Album ${newId}`,
-        description: `Description for new album ${newId}`,
-        img_src: imgSrc,
-        photos: [{ img_src: imgSrc }]
-      });
+    fs.readFile(metadataFilePath, 'utf8', (err, metadata) => {
+      if (err) {
+        console.error('Error reading metadata.json:', err);
+        return;
+      }
+
+      const metadataJson = JSON.parse(metadata);
+      const existingAlbum = jsonData.albums.find(album => album.title === metadataJson.title && album.description === metadataJson.description);
+
+      if (existingAlbum) {
+        // Add new photo to existing album
+        existingAlbum.photos.push({ img_src: imgSrc });
+      } else {
+        // Create new album
+        const newId = jsonData.albums.length + 1;
+        jsonData.albums.push({
+          id: newId,
+          title: metadataJson.title || `New Album ${newId}`,
+          description: metadataJson.description || `Description for new album ${newId}`,
+          img_src: imgSrc,
+          photos: [{ img_src: imgSrc }]
+        });
+      }
 
       fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', err => {
         if (err) {
@@ -35,13 +49,44 @@ function updateAlbumsJson(newImage) {
           console.log('images.json updated successfully.');
         }
       });
-    }
+    });
   });
 }
 
-// Watch for changes in the images folder
-chokidar.watch(imagesFolder).on('add', (filePath) => {
-  const fileName = path.basename(filePath);
-  console.log(`File added: ${fileName}`);
-  updateAlbumsJson(fileName);
-});
+// Function to remove deleted images from images.json
+function removeDeletedImages(deletedImagePath) {
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading images.json:', err);
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    const imgSrc = path.relative(path.join(__dirname, 'public'), deletedImagePath).replace(/\\/g, '/');
+
+    jsonData.albums.forEach(album => {
+      album.photos = album.photos.filter(photo => photo.img_src !== imgSrc);
+    });
+
+    fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', err => {
+      if (err) {
+        console.error('Error writing images.json:', err);
+      } else {
+        console.log('images.json updated successfully.');
+      }
+    });
+  });
+}
+
+// Watch for changes in the images folder and subdirectories
+chokidar.watch(imagesFolder, { persistent: true, ignoreInitial: true, depth: 99 })
+  .on('add', (filePath) => {
+    console.log(`File added: ${filePath}`);
+    updateAlbumsJson(filePath);
+  })
+  .on('unlink', (filePath) => {
+    console.log(`File removed: ${filePath}`);
+    removeDeletedImages(filePath);
+  });
+
+console.log('Script is running...');
