e012c5b6bb2acd95fd5929b74eaf4a328ba2487d
SumithBandela <sumithbandela@gmail.com>
2025-02-18 23:09:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { HashRouter as BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import { Home } from './home';
 import { About } from './about';
 import { Contact } from './contact-us';
 import { NotFound } from './not-found';
-import {QuoteCalc} from './quote-calc';
+import { QuoteCalc } from './quote-calc';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { AlbumView } from './album-view';
-import { ImageView } from './image-view';
 import { Gallery } from './gallery';
+
 function App() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false); // Close the menu after clicking a nav link
+  };
+
   return (
     <div className="container-fluid">
       <BrowserRouter>
-        <Navbar collapseOnSelect expand="lg" bg="white" variant="light" className="shadow-sm">
+        <Navbar 
+          collapseOnSelect 
+          expand="lg" 
+          bg="white" 
+          variant="light" 
+          className="shadow-sm" 
+          expanded={expanded} 
+        >
           <Container>
             {/* Logo Section */}
             <Navbar.Brand as={Link} to="/home">
@@ -22,22 +35,22 @@ function App() {
             </Navbar.Brand>
 
             {/* Navbar Toggle for Mobile */}
-            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Toggle aria-controls="navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
 
             {/* Navbar Links Section */}
             <Navbar.Collapse id="navbar-nav">
               <Nav className="ms-auto">
                 <Nav.Item>
-                  <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/home" onClick={handleNavClick}>Home</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+                  <Nav.Link as={Link} to="/gallery" onClick={handleNavClick}>Gallery</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link as={Link} to="/about">About us</Nav.Link>
+                  <Nav.Link as={Link} to="/about" onClick={handleNavClick}>About us</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link as={Link} to="/contact">Contact us</Nav.Link>
+                  <Nav.Link as={Link} to="/contact" onClick={handleNavClick}>Contact us</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Navbar.Collapse>
@@ -48,16 +61,13 @@ function App() {
         <section>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path='*' element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
             <Route path="home" element={<Home />} />
-            <Route path='/home/:id' element={<ImageView/>}/>
-            <Route path="gallery" element={<Gallery/>}/>
+            <Route path="gallery" element={<Gallery />} />
             <Route path="/gallery/:title" element={<AlbumView />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
-            <Route path='quote' element={<QuoteCalc/>}/>
-           
-            
+            <Route path="quote" element={<QuoteCalc />} />
           </Routes>
         </section>
       </BrowserRouter>
